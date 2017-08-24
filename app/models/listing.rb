fa83e528fe7db67767c804fec0576e6f38258536
macro6461 <mattcroak718@gmail.com>
2017-08-24 16:39:24 -0400

    Done

---
+++ app/models/listing.rb
@@ -0,0 +1,10 @@
+class Listing < ActiveRecord::Base
+ belongs_to :neighborhood
+ belongs_to :host, :class_name => "User"
+
+
+ has_many :reservations
+ has_many :reviews, foreign_key: "reservation_id" 
+
+ has_many :guests, through: :reviews
+end
